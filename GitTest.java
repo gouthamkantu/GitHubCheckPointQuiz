4c0f34b6c8af448d5a72324b7e7531f8ff8e78b8
gouthamkantu <gouthamkantu@gmail.com>
2018-12-03 11:54:39 -0500

    step 3
    method

--- GitTest.java
+++ GitTest.java
@@ -4,7 +4,8 @@ public class GitTest{
 	public GitTest(String sentence){
 		this.sentence = "This is a GitHub test";
 		}
-
-
+		public String toString(){
+			return "This is my GitHub test";
+			}
 
 }
