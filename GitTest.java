af8e2b81b5ded54f78b9f823db7d90aa396155d1
gouthamkantu <gouthamkantu@gmail.com>
2018-12-03 12:02:14 -0500

    step 3
    methord

--- GitTest.java
+++ GitTest.java
@@ -4,6 +4,8 @@ public class GitTest{
 	public GitTest(String sentence){
 		this.sentence = "This is a GitHub test";
 		}
-
+	public String toString(){
+			return "This is my GitHub test";
+			}
 
 }
