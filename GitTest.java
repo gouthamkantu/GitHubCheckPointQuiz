94e69626f71f643ee842f0eb56058d8489c9cb18
gouthamkantu <gouthamkantu@gmail.com>
2018-12-03 12:01:04 -0500

    step 2
    variable, default

--- GitTest.java
+++ GitTest.java
@@ -4,8 +4,6 @@ public class GitTest{
 	public GitTest(String sentence){
 		this.sentence = "This is a GitHub test";
 		}
-		public String toString(){
-			return "This is my GitHub test";
-			}
+
 
 }
